38673dabc038c4e3b309ab9af67b2d7ecbf961d4
DroppingAnvil <50852574+DroppingAnvil@users.noreply.github.com>
2021-11-08 13:37:45 -0600

    Upload project files

---
+++ src/main/java/us/anvildevelopment/v1/utils/database/annotations/Key.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) DroppingAnvil 2020.
+ * All Rights Reserved.
+ */
+
+package us.anvildevelopment.v1.utils.database.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Key {
+}
